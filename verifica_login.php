d6792a2a8523989a60ffa1be250afce1eee414f7
SidSan97 <sidnei1.8santiago@hotmail.com>
2020-04-02 14:09:08 -0300

    Ajustes de login

--- verifica_login.php
+++ verifica_login.php
@@ -1,10 +1,22 @@
 <?php
 /*session_start();*/
+
+if(!isset($_SESSION['usuario']))
+{
+  $_SESSION['nao_autenticado'] = true;
+	/*header('Location: index.php');
+	exit();*/
+}
+
+//VERIFICADOR DE LOGIN
+/*
+//session_start();
+
 if(!$_SESSION['usuario'])
 {
-	header('Location: index.php');
+	header('Location: login.php');
 	exit();
-}
+}*/
 
 //VERIFICADOR DE LOGIN
 
