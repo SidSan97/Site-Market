ea822d1ec85551ebccf4d878057ecf05b40f5906
SidSan97 <sidnei1.8santiago@hotmail.com>
2020-04-04 12:13:13 -0300

    Adição do bootstrap na página de cadastro

--- cadastro.php
+++ cadastro.php
@@ -1,4 +1,72 @@
 <?php
-session_start();
+
 ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="stylesheet" type="text/css" href="css/home.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css.map">
+  <link rel="stylesheet" type="text/css" href="css/tema.css">
+  <link rel="stylesheet" type="text/css" href="css/all.css">
+  <link rel="stylesheet" type="text/css" href="css/xiaomi.css">
+  <link rel="stylesheet" type="text/css" href="css/login.css">
+    <title>Cadastre-se</title>
+</head>
+ <body>
+    
+
+ <section id="rodape">
+      <div class="container">
+        <div class="row">
+          <div class="col-4" style="text-align: center;">
+             <h5>Dúvidas</h5>
+             <a href="#"><p>Central de Atendimento</p></a>
+             <a href="#"><p>Sobre a Entrega</p></a>
+             <a href="#"><p>Troca e Devolução</p></a>
+             <a href="#"><p>Cancelamento</p></a>
+             <a href="#"><p>Política de Privacidade</p></a>
+          </div>
+
+          <div class="col-4" style="text-align: center;">
+            <h5>Serviços</h5>
+            <a href="#"><p>Marketplace</p></a>
+            <a href="#"><p>Baixe nosso App</p></a>
+            <a href="#"><p>Garantia Estendida e Original</p></a>
+            <a href="#"><p>Instalações e Assistências</p></a>
+            <a href="#"><p>Cartão de Crédito</p></a>
+          </div> 
+
+          <div class="col-4" style="text-align: center;" id="RS-icons">
+            <h5>Redes Sociais e Contato</h5>
+            <a href="#"><p><i class="fab fa-facebook-square"></i></p></a>
+            <a href="#"><p><i class="fab fa-instagram"></i></p></a>
+            <a href="#"><p><i class="fab fa-twitter-square"></i></p></a>
+            <a href="https://github.com/SidSan97"><p><i class="fab fa-github-square"></i></p></a>                   
+          </div>
+        </div>
+      </div>
+  </section>
+
+
+    <div id="rodape-inf" style="background-color: rgb(231, 230, 228);"> 
+        <article class="inf-text">
+          <h1>Loja do SidSan</h1>
+          <p>Sidnei Santiago - Todos os direitos reservados</p>
+          <p>Salvador, 06/03/2019</p>
+          <p><i class="far fa-envelope" style="font-size: 14pt;"></i> sidnei1.8santiago@hotmail.com </p> 
+        </article>
+    </div>
+
+    <script src="js/jquery-3.3.1.js"></script>
+    <script src="js/popper.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/script.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+ </body>
+</html>
 
