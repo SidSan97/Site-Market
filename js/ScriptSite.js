7f2d6ec6811a610413beae69acdd836fc5c99936
SidSan97 <sidnei1.8santiago@hotmail.com>
2020-04-14 16:01:53 -0300

    Adição de script estilizando os inputs de senha cadastro

--- js/ScriptSite.js
+++ js/ScriptSite.js
@@ -6,3 +6,13 @@ function valorTotal()
     document.getElementById("total-Qtd").innerHTML = "R$ " + total + ",00";  
     document.getElementById("total-Compra").innerHTML = "R$ " + total + ",00";   
 }
+
+//Script que torna inputs de senha da página de cadastro avermelhados em caso de senha incompativeis
+const fieldMessage = document.querySelector("#senha");
+const fieldMessage2 = document.querySelector("#senha2");
+
+if(fieldMessage !== fieldMessage2) 
+{
+    fieldMessage.classList.add("alert-input");
+    fieldMessage2.classList.add("alert-input");
+}             
